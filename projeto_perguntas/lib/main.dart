898b8cb21a3c337cc9847e001f76d83a11df40e6
Marcos Vasconcellos de Andrade <dcemvasconcellos@dgp.local>
2022-08-02 16:47:27 -0300

    work2ago22

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,1 +1,56 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const PerguntaApp());
+}
+
+class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({Key? key}) : super(key: key);
+
+  @override
+  State<PerguntaApp> createState() => _PerguntaAppState();
+}
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+      print(_perguntaSelecionada);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[_perguntaSelecionada]),
+            ElevatedButton(
+              child: const Text('Resposta 1'),
+              onPressed: _responder,
+            ),
+            ElevatedButton(
+              child: const Text('Resposta 2'),
+              onPressed: _responder,
+            ),
+            ElevatedButton(
+              child: const Text('Resposta 3'),
+              onPressed: _responder,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
